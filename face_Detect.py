0db33bb17a328fe1ea95863c0150ac78ab8d4f7b
Venkateshwar Reddy Jambula <jvr605@gmail.com>
2017-04-29 23:31:56 +0530

    cropping

--- face_Detect.py
+++ face_Detect.py
@@ -1,9 +1,9 @@
 import cv2
 import sys
-import os
+
 # Get user supplied values
 imagePath = sys.argv[1]
-cascPath = "haarcascade.xml"
+cascPath = "haarcascade_frontalface_default.xml"
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
@@ -14,19 +14,26 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Detect faces in the image
 faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-)
+                                     gray,
+                                     scaleFactor=1.1,
+                                     minNeighbors=5,
+                                     minSize=(30, 30),
+                                     flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+                                     )
 
 print("Found {0} faces!".format(len(faces)))
 
 # Draw a rectangle around the faces
-for (x, y, w, h) in faces:
+for i,(x, y, w, h)  in enumerate(faces):
     #cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    cv2.imshow("Faces found", image)
     sub_face = image[y:y+h, x:x+w]
-    cv2.imwrite("face-" + str(x) +str(y)+ ".jpg",sub_face)
+    r = 48.0 / sub_face.shape[1]
+    dim = (48, int(sub_face.shape[0] * r))
+    # perform the actual resizing of the image and show it
+    resized = cv2.resize(sub_face, dim, interpolation = cv2.INTER_AREA)
+
+    print resized.shape
+    cv2.imwrite("face-" + str(i)+".jpg",sub_face)
+
+cv2.imshow("Faces found", resized)
 cv2.waitKey(0)
